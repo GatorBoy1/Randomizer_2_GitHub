a1e7bfea882f176f746cde7981e664c0db7ba09c
Marvin Laucher <marv070@gmail.com>
2016-10-13 15:07:55 -0400

    add how many names to pair as a message being passed in as well as auto focus for curso on load.

--- app.rb
+++ app.rb
@@ -3,7 +3,7 @@ require_relative 'random_pair.rb'
 require_relative 'formatter.rb'
 
 get '/' do
-    erb :get_names, :locals => {:name_pairs =>" ", :message => "", :message1 => ""}
+    erb :get_names, :locals => {:name_pairs =>" ", :message => "", :message1 => "",:how_many =>"How many people are in class today ?"}
 end
 
 post '/names' do
@@ -11,5 +11,5 @@ post '/names' do
      random_names_array = randomizer(names)
      name_pairs = fix(random_names_array)
      #{}"what are my pairs #{name_pairs}"
-      erb :get_names, :locals => {:name_pairs => name_pairs, :message => "Enter a new number of names to pair.", :message1 => "And your random pairs are"}
+      erb :get_names, :locals => {:name_pairs => name_pairs, :message => "Enter a new number of names to pair.", :message1 => "And your random pairs are", :how_many => " "}
   end
